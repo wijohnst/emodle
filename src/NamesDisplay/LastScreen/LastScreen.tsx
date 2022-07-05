6331a26f3aa80e51f422b8b2e87fb78146e71a32
will-johnston-goodyear <will_johnston@goodyear.com>
2022-07-04 21:00:13 -0400

    adds falling emojis package

--- src/NamesDisplay/LastScreen/LastScreen.tsx
+++ src/NamesDisplay/LastScreen/LastScreen.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+//@ts-ignore
+import { FallingEmojis } from "falling-emojis";
+
 type Props = {};
 
 const LastScreen = (props: Props) => {
-  return <div>LastScreen</div>;
+  return (
+    <>
+      <FallingEmojis emoji="🎉" />
+      <FallingEmojis emoji="🐔" />
+      <span>Last Screen</span>
+    </>
+  );
 };
 
 export default LastScreen;
